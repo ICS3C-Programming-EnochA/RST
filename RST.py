a0712f42d819f1283176304458a0103808a6fc81
Enoch Amedjrovi <enoch.amedjrovi@stu.ocsb.ca>
2025-06-03 16:55:03 +0000

    RST.py

---
+++ RST.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Created by: Enoch Amedjrovi
+# Created on: March 2, 2025
+# This program asks the user for their grade level and tells them their percentage
+
+
+print("Hello, Enoch!")
