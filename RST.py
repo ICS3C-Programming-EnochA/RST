9b4ad3ac0c141a90dd54673d5ce2f47e9403af40
Enoch Amedjrovi <enoch.amedjrovi@stu.ocsb.ca>
2025-06-04 13:07:01 +0000

    RST.py

--- RST.py
+++ RST.py
@@ -4,4 +4,29 @@
 # This program asks the user for their grade level and tells them their percentage
 
 
-print("Hello, Enoch!")
+import ugame
+import stage
+
+
+
+def game_scene():
+    #This function is the main game scene
+
+
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    background = stage.Grid(image_bank_background, 10, 8)
+
+
+    game = stage.Stage(ugame.display, 60)
+    game.layers = [background]
+    game.render_block()
+
+
+    while True:
+        # repeat forever, or you turn it off!
+        pass # just a place holder
+
+
+if __name__ == "__main__":
+    game_scene()
+
