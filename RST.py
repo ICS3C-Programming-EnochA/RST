af15b3ba17f376c1304b3445aa349da14182a310
Enoch Amedjrovi <enoch.amedjrovi@stu.ocsb.ca>
2025-06-04 13:18:33 +0000

    RST.py

--- RST.py
+++ RST.py
@@ -9,23 +9,38 @@ import stage
 
 
 
+
 def game_scene():
     #This function is the main game scene
 
-
+    #image banks for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+
+    #sets the background to image 0 in the image bank
+    # and the sie (10x8 tiles of size 16x16)
     background = stage.Grid(image_bank_background, 10, 8)
 
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
+    #create the stage for the background to show up on
+    # and set the frame rate to 60fps
     game = stage.Stage(ugame.display, 60)
-    game.layers = [background]
+    #set the layers, items show up in order
+    game.layers = [ship] + [background]
+    #render the background and initial location of sprite list 
+    #Most likely you will only render background once per scene
     game.render_block()
 
-
+    #repeat forever, game loop
     while True:
-        # repeat forever, or you turn it off!
-        pass # just a place holder
+        # get user input
 
+        # update game logic
+
+        # redraw Sprites
+        game.render_sprites([ship])
+        game.tick()
 
 if __name__ == "__main__":
     game_scene()
