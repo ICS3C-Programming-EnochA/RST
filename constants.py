e940b5f275410f2362bcf628ff0764e15288f9b9
Enoch Amedjrovi <enoch.amedjrovi@stu.ocsb.ca>
2025-06-12 16:31:41 +0000

    constants

--- constants.py
+++ constants.py
@@ -4,37 +4,29 @@
 # This constants file is for Space Alien game
 # CircuitPython screen size is 160x128 and sprites are 16x16
 
-import stage
-
 SCREEN_X = 160
 SCREEN_Y = 128
-SCREEN_GRID_X = 16
+SCREEN_GRID_X = 10
 SCREEN_GRID_Y = 8
 SPRITE_SIZE = 16
 TOTAL_NUMBER_OF_ALIENS = 5
-TOTAL_NUMBER_OF_LASERS = 5
+TOTAL_NUMBER_OF_LASERS = 8
 SHIP_SPEED = 1
 ALIEN_SPEED = 1
+LASER_SPEED = 2
 OFF_SCREEN_X = -100
 OFF_SCREEN_Y = -100
-LASER_SPEED = 2
 OFF_TOP_SCREEN = -1 * SPRITE_SIZE
 OFF_BOTTOM_SCREEN = SCREEN_Y + SPRITE_SIZE
-SPRITE_MOVEMENT_SPEED = 3
-
-MT_GAME_STUDIO_PALETTE = (b'\xf8\x1f\x00\x00\xcey\x00\xff\xf8\x1f\xff\x19\xfc\xe0\xfd\xe0'
-       b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
-
-SCORE_PALETTE = (b'\xf8\x1f\x00\x00\xcey\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
-       b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 2
 
 # Using for button state
-button_state = {
-    "button_up": "up",
-    "button_just_pressed": "just pressed",
-    "button_still_pressed": "still pressed",
-    "button_released": "released",
-}
-# new pallet for red filled text
-RED_PALETTE = (b'\x00\x00\x00\x22\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-               b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+button_state = {"button_up": "up",
+                "button_just_pressed": "just pressed",
+                "button_still_pressed": "still pressed",
+                "button_released": "released"
+                }
+
+RED_PALETTE = (b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+               b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
